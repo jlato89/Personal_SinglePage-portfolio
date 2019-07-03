6219c6d01cc31b89fab0fa1c128a39758a6cbfce
jlatour88 <jlatour88@gmail.com>
2019-07-02 22:52:30 -0500

    fixed syntax error

--- contact_form.php
+++ contact_form.php
@@ -17,12 +17,13 @@ $headers = 'MIME-Version: 1.0' . "rn";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "rn";
 $headers .= 'From:' . $email. "rn"; // Sender's Email
 // $headers .= 'Cc:' . $email. "rn"; //! Carbon copy to Sender
-$template = 
+$template =
+. 'You have Mail!<br/><br/>'
 . 'Name: ' . $name . '<br/>'
 . 'Email: ' . $email . '<br/>'
 . 'Contact No: ' . $phone . '<br/>'
 . 'Company: ' . $company . '<br/>'
-. 'Message: ' . $message . '<br/><br/>'
+. 'Message: ' . $message . '<br/>';
 $sendmessage = "<div style='background-color:#7E7E7E; color:white;'>" . $template . "</div>";
 // Message lines should not exceed 70 characters (PHP rule), so wrap it.
 $sendmessage = wordwrap($sendmessage, 70);
