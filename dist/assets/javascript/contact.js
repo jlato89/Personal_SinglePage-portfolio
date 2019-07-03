ae259f060c9af45d671dcc6790eea2ebac0539d4
jlatour88 <jlatour88@gmail.com>
2019-07-02 21:48:57 -0500

    added contact form backend functionality

---
+++ dist/assets/javascript/contact.js
@@ -0,0 +1,28 @@
+//* Contact form submission
+$("#form-submit").click(function () {
+   console.log('Click detected');
+   var name = $("#form-name").val();
+   var email = $("#form-email").val();
+   var phone = $("#form-phone").val();
+   var company = $("#form-company").val();
+   var message = $("#form-message").val();
+   $("#returnmessage").empty(); // To empty previous error/success message.
+   // Checking for blank fields.
+   if (name == '' || email == '' || message == '') {
+      alert("Please Fill Required Fields");
+   } else {
+      // Returns successful data submission message when the entered information is stored in database.
+      $.post("contact_form.php", {
+         name1: name,
+         email1: email,
+         phone1: phone,
+         company1: company,
+         message1: message
+      }, function (data) {
+         $("#form-return-message").append(data); // Append returned message to message paragraph.
+         if (data == "Your Query has been received, We will contact you soon.") {
+            $("#form")[0].reset(); // To reset form fields on success.
+         }
+      });
+   }
+});
